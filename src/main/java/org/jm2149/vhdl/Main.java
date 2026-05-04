b1d22508bcf18cbd36ad5ab95c5fb6ba0381cedf
Robbert van Dalen <robbert@eigentime.nl>
2026-05-04 11:08:34 +0200

    init

---
+++ src/main/java/org/jm2149/vhdl/Main.java
@@ -0,0 +1,7 @@
+package org.jm2149.vhdl;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to the Java implementation of an YM2149 VHDL spec");
+    }
+}
